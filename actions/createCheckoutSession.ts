cd11eafcfa6d5211c56ad292189236a790a606fa
Ezra Banoba <ezra@Ezras-MacBook-Pro.local>
2024-07-31 01:05:50 -0600

    Connect to stripe, fix firebase issues, add toaster, add stripe webhook

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -40,7 +40,7 @@ export async function createCheckoutSession(userDetails: UserDetails) {
     payment_method_types: ["card"],
     line_items: [
       {
-        price: "price_1Pgn5DC8Alswqr61VAlju4va",
+        price: "price_1P6P1MHhjPF0K2320z6P9WH7",
         quantity: 1,
       },
     ],
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,13 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { FilePlus2 } from "lucide-react";
+import UpgradeButton from "./UpgradeButton";
 
-const Header = () => {
+function Header() {
   return (
     <div className="flex justify-between bg-white shadow-sm p-5 border-b">
-      <Link href="/dashboard" className=" text-2xl">
-        Chat to <span className=" text-indigo-600"> PDF</span>
+      <Link href="/dashboard" className="text-2xl">
+        Chat to <span className="text-indigo-600">PDF</span>
       </Link>
 
       <SignedIn>
@@ -20,18 +21,17 @@ const Header = () => {
             <Link href="/dashboard">My Documents</Link>
           </Button>
 
-          <Button asChild variant="outline" className=" border-indigo-600">
+          <Button asChild variant="outline" className="border-indigo-600">
             <Link href="/dashboard/upload">
-              <FilePlus2 className=" text-indigo-600" />
+              <FilePlus2 className="text-indigo-600" />
             </Link>
           </Button>
 
-          {/* Upgrade Link */}
+          <UpgradeButton />
           <UserButton />
         </div>
       </SignedIn>
     </div>
   );
-};
-
+}
 export default Header;
